2222a879f5c045d38a53b18232adb2dcb9c781ff
samfok <sam.b.fok@gmail.com>
2014-06-11 01:12:30 -0700

    add python multiprocess animation

--- animation/scatter_multiprocess.py
+++ animation/scatter_multiprocess.py
@@ -2,17 +2,16 @@ from numpy.random import rand
 from numpy import zeros
 import matplotlib
 matplotlib.use('Qt4Agg')
+#matplotlib.use('TkAgg')
 from matplotlib import pyplot as plt
 print 'using backend', plt.get_backend()
 from matplotlib import animation
 import time
-import thread
+import multiprocessing as mp
 
-data1 = zeros(3)
-data2 = zeros(3)
 initted = False
 
-def run_gui():
+def run_gui(conn):
     fig = plt.figure(figsize=(16,6))
     ax1 = fig.add_subplot(121)
     ax2 = fig.add_subplot(122)
@@ -31,9 +30,11 @@ def run_gui():
         line1.set_data([.1, .5, .9], [-1, -1, -1])
         line2.set_data([.1, .5, .9], [-1, -1, -1])
         initted = True
-        return line1, line2
+        return ax1, ax2, line1, line2
     
     def animate(i):
+        data1, data2 = conn.recv()
+        print(data1, data2)
         line1.set_data([.1, .5, .9], data1)
         line2.set_data([.1, .5, .9], data2)
         return line1, line2, 
@@ -43,10 +44,20 @@ def run_gui():
     plt.show()
 
 if __name__ == "__main__":
-    thread.start_new_thread(run_gui, ())
+    parent_conn, child_conn = mp.Pipe()
+    gui_proc = mp.Process(target=run_gui, args=(child_conn,))
+    gui_proc.start()
     idx = 0
+    data1 = zeros(3)
+    data2 = zeros(3)
     while True:
-        data1[idx] = rand()
-        data2[idx] = rand()
-        time.sleep(1.)
-        idx = (idx+1) % 3
+        try:
+            data1[idx] = rand()
+            data2[idx] = rand()
+            time.sleep(1.)
+            idx = (idx+1) % 3
+            parent_conn.send([data1, data2])
+        except KeyboardInterrupt:
+            print("exiting")
+            gui_proc.terminate()
+            break
